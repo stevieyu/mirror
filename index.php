c9f5f438e52a5fa1dbd1cc5188eda2a16eb3c46f
Stevie <791369498@qq.com>
2022-07-18 18:33:45 +0800

    init

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+require './vendor/autoload.php';
+
+if(isset($_SERVER['HTTP_IF_NONE_MATCH'])) { 
+    http_response_code(304);
+    exit; 
+}
+
+$startTime = microtime(true);
+
+$client = new \GuzzleHttp\Client();
+
+$origin = 'https://httpbin.org';
+
+$http_type = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https' : 'http';
+$replaceOrigin = $http_type.'://'.$_SERVER['HTTP_HOST'];
+
+//dd($_SERVER);
+
+$args = [
+    'method' => $_SERVER['REQUEST_METHOD'],
+    'uri' => $_SERVER['REQUEST_URI'] ?? $_SERVER['PATH_INFO'],
+    'headers' => [
+        'Accept' => $_SERVER['HTTP_ACCEPT'],
+        'User-Agent' => $_SERVER['HTTP_USER_AGENT'],
+        'Cookie' => $_SERVER['HTTP_COOKIE'],
+    ],
+];
+
+
+$response = $client->request($args['method'], $origin.$args['uri'], [
+    'headers' => $args['headers'],
+]);
+$content = $response->getBody()->getContents();
+if(is_string($content)) $content = str_replace($origin, $replaceOrigin, $content);
+
+header('Server-Timing: app;dur='. round((microtime(true) - $startTime) * 1000, 2));
+$excpt = ['Connection', 'Date'];
+http_response_code($response->getStatusCode());
+foreach ($response->getHeaders() as $key => $value) {
+    if(in_array($key, $excpt)) continue;
+    header($key.': '.implode(' ', $value), true);
+}
+
+echo $content;
+
+
+
+
+
+
+// Send an asynchronous request.
+// $request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org/get');
+
+// $promise = $client->sendAsync($request)->then(function ($response) {
+//     echo 'I completed! ' . $response->getBody();
+// });
+// echo "promise";
+// $promise->wait();
