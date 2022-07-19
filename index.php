5a5811602c4510eab1ca50a1bba603a5cb9baa53
StevieYu <yzm629@sina.cn>
2022-07-19 08:54:44 +0000

    update

--- index.php
+++ index.php
@@ -13,8 +13,6 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');
 }
-
-
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])){
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
