ebada030a2edceb3ad9e7af75525537501f90c7c
StevieYu <yzm629@sina.cn>
2022-09-06 03:09:57 +0000

    update

--- wxauthorize.php
+++ wxauthorize.php
@@ -1,6 +1,10 @@
 <?php declare(strict_types = 1);
 
-function builderRedirectUri(string $uri, array $query): string{
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// ini_set('error_reporting', E_ERROR);
+
+function builderRedirectUri($uri, $query){
 	$parse_url_arr = parse_url($uri);
     $redirect_query_str = $parse_url_arr['query'] ?? '';
     if($redirect_query_str){
@@ -19,7 +23,7 @@ function builderRedirectUri(string $uri, array $query): string{
 
     return $uri;
 }
-function builderAuthorize(array $query): string{
+function builderAuthorize($query){
 	$user_agent = $_SERVER['HTTP_USER_AGENT'] ?? '';
     if (stristr($user_agent, 'MicroMessenger')) {
     	$uri = 'https://open.weixin.qq.com/connect/oauth2/authorize';
@@ -40,12 +44,14 @@ if(!empty($_GET['appid']) && !empty($_GET['response_type']) && !empty($_GET['sco
 	}
 
 	$origin = $_SERVER['HTTP_ORIGIN'] ?? ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? 'http').'://'.$_SERVER['HTTP_HOST'];
-	$query['redirect_uri'] = $origin.$_SERVER['SCRIPT_NAME'];
+	$query['redirect_uri'] = $origin.'/wxauthorize.php';
 
 	http_response_code(301);
     header('Location: '.builderAuthorize($query));
     exit;
 }elseif((!empty($_GET['code']) || isset($_GET['state'])) && !empty($_COOKIE['redirect_uri'])){
+    setcookie('redirect_uri', '', time() - 1);
+
     http_response_code(301);
     header('Location: '.builderRedirectUri($_COOKIE['redirect_uri'], $_GET));
     exit;
