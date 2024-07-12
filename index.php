36ea2257aada8b394fa2d88ea0958751ff2e4031
stevie <yzm791@qq.com>
2024-07-12 07:47:01 +0000

    -

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 date_default_timezone_set('PRC');
 
@@ -23,39 +25,41 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
     header('Access-Control-Max-Age: 86400');
 }
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])){
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     }
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])){
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
     }
-    exit; 
+    exit;
 }
-if(preg_match('/ico$/', $_SERVER['REQUEST_URI'])){
+if (preg_match('/ico$/', $_SERVER['REQUEST_URI'])) {
     exit;
 }
 
 
-if (!function_exists('getallheaders')){
-    function getallheaders(){
-       $headers = [];
-       foreach ($_SERVER as $name => $value) {
-           if (substr($name, 0, 5) == 'HTTP_') {
-               $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
-           }
-       }
-       return $headers;
+if (!function_exists('getallheaders')) {
+    function getallheaders()
+    {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
     }
 }
 
-function fetch($url, $options){
+function fetch($url, $options)
+{
     $stack = \GuzzleHttp\HandlerStack::create();
     $stack->push(new \Kevinrob\GuzzleCache\CacheMiddleware(
         new \Kevinrob\GuzzleCache\Strategy\GreedyCacheStrategy(
             new \Kevinrob\GuzzleCache\Storage\FlysystemStorage(
                 new \League\Flysystem\Local\LocalFilesystemAdapter(sys_get_temp_dir())
             ),
-            60,
+            60 * 60,
             new \Kevinrob\GuzzleCache\KeyValueHttpHeader(['Authorization'])
         )
     ), 'cache');
@@ -77,11 +81,12 @@ function fetch($url, $options){
 
     return $response;
 }
-function setCookieFromHeader(string $cookieHeader): void {
-    $parts = explode(';',$cookieHeader);
+function setCookieFromHeader(string $cookieHeader): void
+{
+    $parts = explode(';', $cookieHeader);
     $cookieParams = array_shift($parts); // 获取name=value对
     $cookieArray = match (true) {
-        str_contains($cookieParams, '=') => explode('=',$cookieParams, 2),
+        str_contains($cookieParams, '=') => explode('=', $cookieParams, 2),
         default => ['', $cookieParams],
     };
 
@@ -97,8 +102,8 @@ function setCookieFromHeader(string $cookieHeader): void {
     ];
     unset($cookieArray[0]);
     unset($cookieArray[1]);
- 
-    foreach ($parts as$part) {
+
+    foreach ($parts as $part) {
         $part = trim($part);
         if (str_starts_with($part, 'expires=')) {
             $cookieArray['expires'] = strtotime(substr($part, 8));
@@ -140,9 +145,11 @@ function setCookieFromHeader(string $cookieHeader): void {
         );
     }
 }
-function URL($raw){
+function URL($raw)
+{
+
     $url = parse_url($raw);
-    if(!$url || empty($url['host']) || !strstr($url['host'], '.') || !count(dns_get_record($url['host'], DNS_A))) return false;
+    if (!$url || empty($url['host']) || !preg_match('/\w+\.\w+$/', $url['host']) || !checkdnsrr($url['host'], 'A')) return false;
 
     return array_merge(
         $url,
@@ -157,12 +164,13 @@ function URL($raw){
         ]
     );
 }
-function filterM3U8NotSort($content) {
+function filterM3U8NotSort($content)
+{
     $prev = 0;
     $isSort = true;
-    $matches = array_values(preg_grep('/\w+.ts/', explode("\n",$content)));
+    $matches = array_values(preg_grep('/\w+.ts/', explode("\n", $content)));
 
-    return array_values(array_filter($matches, function($i,$idx) use (&$prev, &$isSort) {
+    $ads = array_values(array_filter($matches, function ($i, $idx) use (&$prev, &$isSort) {
         $current = intval(preg_replace('/.*?(\d+).ts/', '$1', $i));
         $isNotSort = ($current - $prev) != 1;
 
@@ -170,7 +178,7 @@ function filterM3U8NotSort($content) {
             $prev = $current;
         }
 
-        if ($idx == 1 &&$isNotSort) {
+        if ($idx == 1 && $isNotSort) {
             $isSort = false;
         }
 
@@ -178,55 +186,67 @@ function filterM3U8NotSort($content) {
             return false;
         }
 
-        return $idx > 0 &&$isNotSort;
+        return $idx > 0 && $isNotSort;
     }, ARRAY_FILTER_USE_BOTH));
+
+    if (count($ads)) {
+        $regex = '/.*?\s' . generateRegexpFromStrings($ads) . '\s/';
+        $content = preg_replace($regex, '', $content);
+    }
+
+    return $content;
 }
-function generateRegexpFromStrings($array){
-    $baseString =$array[0];
-    $commonString =$baseString;
+function generateRegexpFromStrings($array)
+{
+    $baseString = $array[0];
+    $commonString = $baseString;
 
     foreach ($array as $str) {
-        for ($i = 0;$i < strlen($baseString) &&$i < strlen($str);$i++) {
+        for ($i = 0; $i < strlen($baseString) && $i < strlen($str); $i++) {
             if ($baseString[$i] != $str[$i]) {
                 $commonString[$i] = '*';
             }
         }
     }
-    return preg_replace_callback('/\*+/', function($match) {
+    return preg_replace_callback('/\*+/', function ($match) {
         $len = strlen($match[0]);
-        return '\w'.($len > 1 ? '{'.$len.'}' : '');
+        return '\w' . ($len > 1 ? '{' . $len . '}' : '');
     }, $commonString);
 }
-function removeM3U8Ads($content, $list) {
-    $regex = '/.*?\s'.generateRegexpFromStrings($list).'\s/';
-    $content = preg_replace($regex, '', $content);
 
-    return $content;
-}
-function m3u8Handler($content, $url, $host){
+function m3u8Handler($content, $url, $host)
+{
     // dd($url, $content);
-    if(preg_match('/http[^#$]+\.m3u8/', $content)){
-        $content = preg_replace('{https:\\\/([^#$]+\.m3u8)}', 'https:\/\/'.$host.'$1', $content);
+    if (preg_match('/http[^#$]+\.m3u8/', $content)) {
+        $content = preg_replace('{https:\\\/([^#$]+\.m3u8)}', 'https:\/\/' . $host . '$1', $content);
         return $content;
     }
-    if($url['ext'] == 'm3u8') {
+
+    if ($url['ext'] == 'm3u8') {
         //统一内容路径, 绝对转相对
-        $content = str_replace($url['dir'].'/', '', $content);
+        $content = str_replace($url['dir'] . '/', '', $content);
 
-        if(preg_match('/\.ts/', $content)){
-            //移除绝对路径
-            $content = preg_replace('/.*?\s\/\S+\s/', '', $content);
-            //ukzy
-            $content = preg_replace('/#EXT-X-K.*?\s(.*\s)*?.*?Y\s/', '', $content);
+        if (preg_match('/\.ts/', $content)) {
 
-           $ads = filterM3U8NotSort($content);
-            if(count($ads)) {
-                    $content = removeM3U8Ads($content, $ads);
-            }
+            $content = preg_replace([
+                //移除绝对路径
+                '/.*?\s\/\S+\s/',
+                //带key的无分片块
+                '/(#EXT-X-DIS.*?\s#EXT-X-KEY.*?\s){2,}/',
+            ], '', $content);
+
+            $content = filterM3U8NotSort($content);
+
+            // $content = preg_replace([
+
+            // ], '', $content);
+            
         }
-        // dd($content);
+        echo $content;
+        dd($url);
         return $content;
     }
+    dd($url);
     return $content;
 }
 
@@ -235,12 +255,12 @@ $logStore = new \SleekDB\Store("log", sys_get_temp_dir(), [
     // "cache_lifetime" => 60 * 60 * 24 * 7,
 ]);
 // $logStore->insert($article);
- // $logStore->findAll();
+// $logStore->findAll();
 
 
-if(preg_match('/^\/(\?.*)?$/', $_SERVER['REQUEST_URI'])){
+if (preg_match('/^\/(\?.*)?$/', $_SERVER['REQUEST_URI'])) {
     header('Content-Type: application/json');
-    echo json_encode($logStore->findAll(['_id'=>'desc'], 5));
+    echo json_encode($logStore->findAll(['_id' => 'desc'], 5));
     exit;
 }
 
@@ -258,31 +278,35 @@ $args['method'] = $_SERVER['REQUEST_METHOD'];
 $args['body'] = file_get_contents('php://input');
 
 $args['url'] = preg_replace('/^\//', '', $_SERVER['REQUEST_URI'] ?? '');
-$args['url'] = URL(preg_match('/^https?:\/\//', $args['url']) ? $args['url'] : 'https://'.$args['url']);
-if(!$args['url'] && !empty($_SERVER['HTTP_REFERER'])) {
+$args['url'] = preg_match('/^https?:\/\//', $args['url']) ? $args['url'] : 'https://' . $args['url'];
+$args['url'] = URL($args['url']);
+if (!$args['url'] && !empty($_SERVER['HTTP_REFERER'])) {
     $refererUrl = URL($_SERVER['HTTP_REFERER'] ?? '');
-    
-    if($refererUrl && $refererUrl['path'] && $refererUrl['path'] != '/') {
-        $refererUrl = 'https://'.preg_replace('/\/([^\/]+)(?:.*)?/', '$1', $refererUrl['path']).$_SERVER['REQUEST_URI'];
+
+    if ($refererUrl && $refererUrl['path'] && $refererUrl['path'] != '/') {
+        $refererUrl = 'https://' . preg_replace('/\/([^\/]+)(?:.*)?/', '$1', $refererUrl['path']) . $_SERVER['REQUEST_URI'];
         $args['url'] = URL($refererUrl);
     }
 }
-if(!$args['url']){
-    $args['url'] = URL($_COOKIE['_to'].$_SERVER['REQUEST_URI']);
+if(!$args['url'] && $_COOKIE['_to'] ?? '') {
+    if (!$args['url']) {
+        $args['url'] = URL($_COOKIE['_to'] . $_SERVER['REQUEST_URI']);
+    }
+    if ($_COOKIE['_to'] != $args['url']['origin']) {
+        setcookie('_to', $args['url']['origin'], 0, '/');
+    }
 }
-if(!$args['url']) {
+
+if (!$args['url']) {
     $args['url'] = URL('https://httpbin.org/anything');
 }
 
-if(($_COOKIE['_to'] ?? '') != $args['url']['origin']){
-    setcookie('_to', $args['url']['origin'], 0, '/');
-}
 
 
 
 $args['headers'] = array_filter(
     array_merge(
-        getallheaders(), 
+        getallheaders(),
         [
             'Origin' => $args['url']['origin'],
             'Host' => $args['url']['host'],
@@ -290,8 +314,8 @@ $args['headers'] = array_filter(
             'Referer' => str_replace($_SERVER['HTTP_HOST'], $args['url']['host'], $_SERVER['HTTP_REFERER'] ?? $args['url']['origin']),
             'Accept-Encoding' => 'gzip, deflate',
         ]
-    ), 
-    fn($v, $k) => $v,
+    ),
+    fn ($v, $k) => $v,
     // fn($v, $k) => $v && strstr('Authorization,Accept,User-Agent,Cookie,Content-Type,Host,Referer,Accept-Encoding,Cache-Control,Accept-Language', $k),
     ARRAY_FILTER_USE_BOTH
 );
@@ -301,9 +325,9 @@ $log['request'] = $args;
 
 
 $jumpExts = 'ts|zip|gz|bz2|rar|7z|tar|xz|mp4|mp3';
-if($args['url']['ext'] && str_contains($jumpExts, $args['url']['ext'])) {
+if ($args['url']['ext'] && str_contains($jumpExts, $args['url']['ext'])) {
     http_response_code(308);
-    header('Location: '.$args['url']['raw']);
+    header('Location: ' . $args['url']['raw']);
     exit;
 }
 
@@ -313,9 +337,9 @@ $noTxtExts = 'ttf|woff|woff2';
 $isContentTxt = $args['url']['ext'] && str_contains($txtExts, $args['url']['ext']);
 
 
-if($args['method'] == 'GET' && $args['url']['ext'] && !$isContentTxt && !str_contains($noTxtExts, $args['url']['ext'])) {
+if ($args['method'] == 'GET' && $args['url']['ext'] && !$isContentTxt && !str_contains($noTxtExts, $args['url']['ext'])) {
     $response = fetch($args['url']['raw'], array_merge($args, [
-        'method' => 'HEAD',                                             
+        'method' => 'HEAD',
     ]));
     $isContentTxt =  preg_match('/text\/|\/json/', implode($response->getHeader('Content-Type')));
 }
@@ -336,22 +360,22 @@ $logStore->insert($log);
 
 
 
-if(!$args['url']['ext'] || $isContentTxt) {
+if (!$args['url']['ext'] || $isContentTxt) {
     $content = preg_replace(
-        '/\/'.$args['url']['host'].'/', 
-        '/'.getallheaders()['Host'], 
+        '/\/' . $args['url']['host'] . '/',
+        '/' . getallheaders()['Host'],
         $content
     );
     $content = preg_replace(
-        '/"(\/.*?m?js)/', 
-        '"/'.$args['url']['host'].'$1', 
+        '/"(\/.*?m?js)/',
+        '"/' . $args['url']['host'] . '$1',
         $content
     );
     $content = m3u8Handler($content, $args['url'], $_SERVER['HTTP_HOST']);
 }
 
 http_response_code($response->getStatusCode());
-header('Server-Timing: request;dur='. round((microtime(true) - $startTime) * 1000, 2));
+header('Server-Timing: request;dur=' . round((microtime(true) - $startTime) * 1000, 2));
 
 
 foreach ($response->getHeader('Set-Cookie') as $key => $value) {
@@ -362,9 +386,8 @@ foreach ($response->getHeader('Set-Cookie') as $key => $value) {
 $only = ['Content-Type', 'Cache-Control', 'Etag', 'Last-Modified', 'X-Kevinrob-Cache'];
 foreach ($only as $key) {
     foreach ($response->getHeader($key) as $v) {
-        header($key.': '.$v);
+        header($key . ': ' . $v);
     }
 }
 
 echo $content;
-
