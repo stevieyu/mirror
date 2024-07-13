cc60e1ca40ec4024dbc9b156b0ea1e1a6d416a5a
stevie <yzm791@qq.com>
2024-07-13 00:20:34 +0000

    55

--- index.php
+++ index.php
@@ -340,7 +340,7 @@ $args['headers'] = array_filter(
 $log['request'] = $args;
 
 
-$jumpExts = 'ts|zip|gz|bz2|rar|7z|tar|xz|mp4|mp3';
+$jumpExts = 'ts|zip|gz|bz2|rar|7z|tar|xz|mp4|mp3|ttf|woff|woff2';
 if ($args['url']['ext'] && str_contains($jumpExts, $args['url']['ext'])) {
     http_response_code(308);
     header('Location: ' . $args['url']['raw']);
